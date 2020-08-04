503f075a993a6c9371b59eca533a30fa1a4e4e2a
bruno wandrey <bruno.wandrey@escriba.com.br>
2020-08-04 00:30:49 -0300

    follows mouse

--- main.py
+++ main.py
@@ -12,26 +12,43 @@ def main():
     pygame.display.set_caption("minimal program")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((600,600))
+    screen = pygame.display.set_mode((1200,1200))
     bgd_image = pygame.image.load("bg.png")
+    bgchardirty = pygame.image.load("bgchardirty.png")
+
     # define a variable to control the main loop
     running = True
 
     xpos = 1
     ypos = 1
     frame = 0
-    partiture = pygame.image.load("partitura.png")
-    screen.blit(partiture,(xpos,ypos))
+    character = pygame.image.load("char.png")
+    screen.blit(character,(xpos,ypos))
     pygame.display.flip()
-
+    lastMousePosition = (0,0)
+    charPosition = (0,0)
+    
     # main loop
     while running:
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             frame = frame +1
-            screen.blit(bgd_image, (0,0))
-            screen.blit(partiture,(250+math.sin(frame/360)*150,250+math.cos(frame/360)*150))
+            mousePosition = pygame.mouse.get_pos()
+            print(charPosition)
+            screen.blit(bgchardirty, lastMousePosition)
+            if frame%2==0: 
+                if charPosition[0] < mousePosition[0]:
+                    charPosition = (charPosition[0] +1 , charPosition[1])
+                else:
+                    charPosition = (charPosition[0] -1 , charPosition[1])
+                if charPosition[1] < mousePosition[1]:
+                    charPosition = (charPosition[0]  , charPosition[1]+1)
+                else:
+                    charPosition = (charPosition[0] , charPosition[1]-1)
+
+            screen.blit(character, charPosition)
             pygame.display.flip()
+            lastMousePosition = mousePosition
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
